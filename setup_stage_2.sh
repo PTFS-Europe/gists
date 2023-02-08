c61aa7b72f8575bf6d9eb7462655ad9a7f2bebc2
?/θ <jake.deery@ptfs-europe.com>
2023-02-08 12:20:32 +0000

    Update setup_stage_2.sh

--- setup_stage_2.sh
+++ setup_stage_2.sh
@@ -24,3 +24,12 @@ sudo -E ${HOME}/scripts/sys/setup_initial_system.sh
 ## say bye
 echo "Done! Please now install the relevant application this server is intended"
 echo "to run."
+
+echo -ne "VERY IMPORTANT!\n"
+cat <<EOF
+   ___ _           _     _   _                   _             _   _
+  / __| |_  ___ __| |__ | |_| |_  ___   ___ _  _| |_ _ __ _  _| |_| |
+ | (__| ' \/ -_) _| / / |  _| ' \/ -_) / _ \ || |  _| '_ \ || |  _|_|
+  \___|_||_\___\__|_\_\  \__|_||_\___| \___/\_,_|\__| .__/\_,_|\__(_)
+                                                    |_|
+EOF
